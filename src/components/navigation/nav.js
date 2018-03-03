142b0dad59ad0a8f8e20b0bc4aaf90d35ff9170c
Douglas Soule <souleman13@gmail.com>
2018-03-03 09:43:00 -0700

    basic auth setup, more comments

--- src/components/navigation/nav.js
+++ src/components/navigation/nav.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 
 //code for any sort of navigation should go in this file,
 //or in an addition file within the same folder
+//ex. topnav and side nav would likely be different files
 
 export default class extends Component {
     render(){
--- src/config/apollo.js
+++ src/config/apollo.js
@@ -1,10 +1,28 @@
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
 
 //apollo configuration here
+const endpoint = new HttpLink({ uri: '__YOUR__GRAPHQL__ENDPOINT__' })
+
+//basic auth setup
+//graphql/apollo tends to set up auth on a per-request basis
+//in which you must get the auth token from local storage and 
+//attach it to the header of each request
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+})
 
 export const client = new ApolloClient({
-    link: new HttpLink({ uri: '__YOUR__GRAPHQL__ENDPOINT__' }),
+    link: authLink.concat(endpoint),
     cache: new InMemoryCache()
   })
